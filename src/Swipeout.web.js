224672524a11637013455078aada530061f7771a
Justin Bailey <meticoeus@gmail.com>
2018-07-16 01:18:50 -0400

    Change PropTypes to prop-type packages

--- src/Swipeout.web.js
+++ src/Swipeout.web.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import Hammer from 'react-hammerjs';
 import omit from 'object.omit';
 import splitObject from './util/splitObject';
